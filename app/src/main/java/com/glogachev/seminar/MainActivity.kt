b4d0eb6996b2e27c16d57e0baef163d83d632213
GlebLogachev <GlebLogachev@users.noreply.github.com>
2021-08-27 10:55:25 +0700

    commit 1: демонстрация ошибок

--- app/src/main/java/com/glogachev/seminar/MainActivity.kt
+++ app/src/main/java/com/glogachev/seminar/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.glogachev.seminar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.glogachev.seminar.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnDownload.setOnClickListener {
+                loadLargeFile()
+            }
+            btnUpdateUi.setOnClickListener {
+                updateUiInMainThread()
+            }
+
+            btnRight.setOnClickListener {
+                updateUi()
+            }
+        }
+    }
+
+    // anr error
+    private fun loadLargeFile() {
+        TimeUnit.SECONDS.sleep(20)
+    }
+
+    // crash app
+    private fun updateUiInMainThread() {
+        Thread {
+            binding.tvExampleText.text = "try to updateUI"
+        }.start()
+    }
+
+    private fun updateUi() {
+        Thread {
+            TimeUnit.SECONDS.sleep(5)
+            binding.tvExampleText.post {
+                binding.tvExampleText.text = "success"
+            }
+        }.start()
     }
 }

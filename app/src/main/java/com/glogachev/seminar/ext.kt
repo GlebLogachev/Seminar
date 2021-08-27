9a6e7a150b22dbc8507d6698b3d663424848d586
GlebLogachev <GlebLogachev@users.noreply.github.com>
2021-08-27 22:33:40 +0700

    осмысленный коммит

---
+++ app/src/main/java/com/glogachev/seminar/ext.kt
@@ -0,0 +1,22 @@
+package com.glogachev.seminar
+
+import android.graphics.Color
+
+val colors: Array<Int> = arrayOf(
+    Color.BLACK,
+    Color.BLUE,
+    Color.CYAN,
+    Color.GREEN,
+    Color.RED,
+    Color.TRANSPARENT,
+    Color.YELLOW,
+    Color.DKGRAY,
+    Color.GRAY
+)
+const val DELAY = 2000L
+const val TOKEN = 1
+const val TAG = "myTag"
+
+const val TOGGLE_MESSAGE = 1
+const val NEXT_COLOR_MESSAGE = 2
+const val TOAST_MESSAGE = 3
